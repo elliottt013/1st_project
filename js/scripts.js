8e05e09cb48a48ee54d17ec8ef19e5ab786ed7ea
Elliottt_013 <akhmed_tochiev@mail.ru>
2021-01-01 18:15:42 +0300

    first commit dohun

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let name6 = 'mma';
+
+  
+
+let user = {
+    name: 'Akhmad',
+    age: 24,
+    gender:  'male'
+};
+
+console.log(user.age);
+
+let avto = ['bmw', 'lada', 'kamaz'];
+console.log(avto[1], name6);
